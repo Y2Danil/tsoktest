639603269c25068f26d60f3c46d1d582e22c4f55
Y2Danil <mc_danil@inbox.ru>
2020-06-25 21:24:41 +1200

    upgrade register and login

--- Top/static/js/TSOK.js
+++ Top/static/js/TSOK.js
@@ -5,6 +5,13 @@ jQuery('base.html').ready(function(){
       'border-color': 'none !important'
     });
   });
+  $('.register form input').after('<br>');
+  $('.register form input[type=text]').attr('placeholder', "Имя пользователя");
+  $('.register form input[name=password1]').attr('placeholder', "Пароль");
+  $('.register form input[name=password2]').attr('placeholder', "Потвердить пароль");
+
+  $('.login form input[type=text]').attr('placeholder', "Имя пользователя");
+  $('.login form input[type=password]').attr('placeholder', "Пароль");
 });
  
 new Vue ({
