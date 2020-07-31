5bb051ded77a01beb02efe767d0cdc586da98baa
Y2Danil <mc_danil@inbox.ru>
2020-07-31 12:49:21 +0300

    Небольшиу правки

--- TSOK/wsgi.py
+++ TSOK/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 #from whitenoise import WhiteNoise
 
-s.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TSOK.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TSOK.settings')
 
-#application = get_wsgi_application()
+application = get_wsgi_application()
 #application = WhiteNoise(application)
